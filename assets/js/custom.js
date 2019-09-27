a2478411ae2dde2afbbec92f416c184e09ef2332
Rizky Syaiful <rizky.syaiful@gmail.com>
2019-09-27 15:14:28 +0700

    to be reviewed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,6 +6,19 @@ $(document).ready(function(){
 		offset: 75
 	});
 
+	// Collapse Navbar
+	var navbarCollapse = function() {
+		if ($("#mainNav").offset().top > 100 ) {
+			$("#mainNav").addClass("navbar-scrolled");
+		} else {
+			$("#mainNav").removeClass("navbar-scrolled");
+		}
+	};
+	// Collapse now if page is not at top
+	navbarCollapse();
+	// Collapse the navbar when page is scrolled
+	$(window).scroll(navbarCollapse);
+
 	// Smooth scrolling 
 	$(function(){
 		$('a.nav-link[href*="#"]').click(function() {
@@ -28,5 +41,6 @@ $(document).ready(function(){
 			$(this).find('svg').attr('data-icon', 'plus-square');
 		};
 	});
+
 });
 
