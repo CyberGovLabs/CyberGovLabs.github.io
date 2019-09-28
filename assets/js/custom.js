5208a152c0cba0c82d165936e5129a3793b48613
Rizky Syaiful <rizky.syaiful@gmail.com>
2019-09-28 11:08:51 +0700

    link to wiki on navmenu, etc

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
 
 	// Smooth scrolling 
 	$(function(){
-		$('a.nav-link[href*="#"]').click(function() {
+		$('a.tracked[href*="#"]').click(function() {
 			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 				var $target = $(this.hash);
 				$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
